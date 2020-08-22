94a984e74c35c24661036709cc8a986c6d3df145
vfrmn <vitaliy.furman19@gmail.com>
2020-08-22 17:01:41 +0300

    update config

--- config/app.php
+++ config/app.php
@@ -165,7 +165,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Barryvdh\Debugbar\ServiceProvider::class,
         /*
          * Application Service Providers...
          */
